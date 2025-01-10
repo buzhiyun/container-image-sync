98c9ff4e97fb7e16a99a4b151c329caf43b1876d
buzhiyun <buzhiyun@sina.com>
2025-01-10 16:53:21 +0800

    不再检查关键词

--- start.sh
+++ start.sh
@@ -9,19 +9,22 @@ sudo chmod +x kubeadm
 
 k8s_image_list=$(./kubeadm config images list -o text --skip-headers --kubernetes-version ${RELEASE})
 # 定义重点镜像的关键词列表
-keywords=("kube-apiserver" "kube-controller-manager" "kube-scheduler" "kube-proxy" "pause" "etcd")
+# keywords=("kube-apiserver" "kube-controller-manager" "kube-scheduler" "kube-proxy" "pause" "etcd")
 
 
 # 遍历镜像列表
 for image in $k8s_image_list; do
     # 检查镜像名称是否包含关键词
-    if [[ "$image" == *"$keyword"* ]]; then
-        # 拉取镜像
-        echo "Pulling image: $image"
-        docker pull $image
-        new_image=$(echo $image | awk -F'/' '{print "${NEW_REPOSITORIES}/"$2"/"$3}')
-        echo "Tagging image: $image"
-        docker tag $image $new_image
-        echo "Pushing image: $new_image"
-    fi
+    # if [[ "$image" == *"$keyword"* ]]; then
+
+    # 拉取镜像
+    echo "Pulling image: $image"
+    docker pull $image
+    new_image=$(echo $image | awk -F'/' '{print "${NEW_REPOSITORIES}/"$2"/"$3}')
+    echo "Tagging image: $image"
+    docker tag $image $new_image
+    echo "Pushing image: $new_image"
+
+
+    # fi
 done
