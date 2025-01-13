2d18c92c545cb6d70e99a48742f42423c21a0a20
buzhiyun <buzhiyun@sina.com>
2025-01-13 14:19:08 +0800

    修复空格造成的bug

--- start.sh
+++ start.sh
@@ -8,6 +8,6 @@ docker pull ${IMAGE}
 new_image=${MY_REGISTRY}/$(echo "${IMAGE}" | sed 's/mcr.microsoft.com/mcr/g' | sed 's/quay.io/quay/g')
 
 echo "Tagging image: ${IMAGE}"
-docker tag ${IMAGE}$new_image
+docker tag ${IMAGE} $new_image
 echo "Pushing image: $new_image"
 docker push $new_image
