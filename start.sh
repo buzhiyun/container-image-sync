13eeb31171ac2db35e919770a55d1f475fac6e73
不二周助 <buzhiyun@sina.com>
2025-02-26 17:10:05 +0800

    Update start.sh
    取消mcr quay特殊标注

--- start.sh
+++ start.sh
@@ -5,7 +5,8 @@
 echo "Pulling image: ${IMAGE}"
 docker pull ${IMAGE}
 # new_image=${MY_REGISTRY}/$(echo $IMAGE | awk -F'/' '{for(i=2;i<=NF;i++) printf "%s/", $i; print ""}' | sed 's/\/$//')
-new_image=${MY_REGISTRY}/$(echo "${IMAGE}" | sed 's/mcr.microsoft.com/mcr/g' | sed 's/quay.io/quay/g')
+# new_image=${MY_REGISTRY}/$(echo "${IMAGE}" | sed 's/mcr.microsoft.com/mcr/g' | sed 's/quay.io/quay/g')
+new_image=${MY_REGISTRY}/${IMAGE}
 
 echo "Tagging image: ${IMAGE}"
 docker tag ${IMAGE} $new_image
